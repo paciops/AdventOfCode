c17cfde4516159a925036fe3626919a6449b7402
Riccardo <riccardo960@gmail.com>
2023-01-05 00:02:57 +0100

    Solved day 8

--- 2022/08/treetop.jl
+++ 2022/08/treetop.jl
@@ -25,7 +25,29 @@ function visible((row, col), field, (m, n))
     return (ismax(field, 1:row, col, row, findfirst)) || (ismax(field, row:m, col, 1, findlast)) || (ismax(field, row, 1:col, col, findfirst)) || (ismax(field, row, col:n, 1, findlast))
 end
 
-function main(filename)
+function calcdist(field, (row, col), fun)
+    list = field[row, col] |> fun
+    l = length(list)
+    el = list[1]
+    dist = 0
+    for i in 2:l
+        dist += 1
+        if list[i] >= el
+            break
+        end
+    end
+    return dist
+end
+
+function calcscenicscore(field, (row, col), (m, n))
+    top = calcdist(field, (1:row, col), reverse)
+    bottom = calcdist(field, (row:m, col), identity)
+    left = calcdist(field, (row, 1:col), reverse)
+    right = calcdist(field, (row, col:n), identity)
+    return top * bottom * left * right
+end
+
+function main(filename, printfield = false)
     field = creatematrix(filename)
     # all numbers are in field
     #println(typeof(field))
@@ -33,16 +55,27 @@ function main(filename)
     # trees on the border
     total = m * 2 + (n-2)*2
     # trees on the inside
-    println("Field:")
-    eachrow(field) .|> println
+    printfield && println("Field:")
+    printfield && eachrow(field) .|> println
+    bestscenicscore = 0
     for row in 2 : m-1
         for col in 2 : n-1
-            total += visible((row, col), field, (m, n)) ? 1 : 0 
+            v = visible((row, col), field, (m, n))
+            total += v ? 1 : 0 
+            if v
+                scenicscore = calcscenicscore(field, (row, col), (m, n))
+                if scenicscore > bestscenicscore
+                    bestscenicscore = scenicscore
+                end
+            end
         end 
     end
-    return total
+    print(filename,  "\tvisible trees = ", total)
+    print("\t scenic score = ", bestscenicscore)
+    println("\t size = ", (m,n))
+    return
 end
 
 for arg in ARGS
-    println(arg, " -> ", main(arg))
+    main(arg)
 end
