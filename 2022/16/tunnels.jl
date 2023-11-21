11f1f9c6648fec6a0b398c8d32eb1955dd952455
riccardo pacioni <riccardo960@gmail.com>
2023-11-21 18:20:23 +0100

    solution still not found

--- 2022/16/tunnels.jl
+++ 2022/16/tunnels.jl
@@ -8,13 +8,19 @@ function parseValve(data::Vector{String})
     return replace.(data[valves+1:end], "," => "")
 end
 
-struct Node
+struct Node{T}
     flow::Int
-    valves::Array{String}
+    valves::Array{T}
 end
 
+struct Graph{T}
+
+end
+
+NodeMap{T} = Dict{String, Node{T}} where T
+
 function parseInput(filename::String)
-    map = Dict{String, Node}()
+    map = NodeMap{String}()
     start = nothing
     open(filename) do file
         for line in eachline(file)
@@ -23,86 +29,116 @@ function parseInput(filename::String)
             start = isnothing(start) ? name : start
             flow = parse(Int, replace(data[5], ";"=>" ", "rate="=>" "))
             valves = parseValve(data)
-            node = Node(flow, valves)
+            node = Node{String}(flow, valves)
             map[name] = node
         end
     end
     return map, start
 end
 
-function sortfn(a::Tuple{String, Tuple{Int, Int}}, b::Tuple{String, Tuple{Int, Int}})
-    if a[2][1] == b[2][1]
-        return isless(a[2][2], b[2][2])
-    else
-        return !isless(a[2][1], b[2][1])
+function bfs(map::NodeMap{T}, start::T) where T
+    visited = Set{T}()
+    queue = Vector([start])
+    distances = Dict{T, Int}(start => 0)
+    while !isempty(queue)
+        curr = popfirst!(queue)
+        distance = distances[curr]+1
+        for k in map[curr].valves
+            if !haskey(distances, k)
+                distances[k] = distance
+                push!(queue, k)
+            end
+        end
     end
+    return distances
 end
 
-function nextnode(key::String, map::Dict{String, Node}, open::Set{String})
-    flows = Dict{String, Tuple{Int, Int}}(key => (0, 0))
-    queue = [key]
-    visited = Set()
-    bestflow = -1
-    index = ""
-    while !isempty(queue)
-        current_node = popfirst!(queue)
-        _, current_distance = flows[current_node]
-        push!(visited, current_node)
-
-        for k in filter(el -> !(el in visited), map[current_node].valves)
-            f = map[k].flow - current_distance + 1
-            if k in open
-                f = current_distance + 1
-            end
-            flows[k] = f, current_distance + 1
-            if f > bestflow
-                bestflow = f
+function findbest(map, distances, key, opened)
+    best = 0.0
+    index = nothing
+    for (k, dist) in distances
+        if !(k in opened)
+            valve = map[k]
+            d = distances[key][k]
+            d = d == 0 ? 1 : d
+            if valve.flow / d > best
+                best = valve.flow / d
                 index = k
             end
-            push!(queue, k)
         end
     end
     return index
-end
-
-function notthebest(n::Int, next::String, map::Dict{String, Node}, seconds::Int, verbose:: Bool)
-    open = Set{String}()
-    total = 0
-    sum = 0
-    sequence = Vector{String}()
-    for i = 1:seconds
-        push!(sequence, next)
-        verbose && println("== Minute $(i) ==")
-        msg = length(open) > 0 ? "Valves $(join(open, ", ")) are open" : "No valves are open"
-        msg *= sum > 0 ? ", releasing $(sum) pressure." : ""
-        verbose && println(msg)
-        key = next
-        curr = map[key]
-        if curr.flow == 0 || key in open
-            if n != length(open)
-                next = nextnode(key, map, open)
-                verbose && println("You move to $(next)")
-            end
-        else
-            verbose && println("You open valve $(key)")
-            sum += curr.flow
-            total += curr.flow * (seconds-i)
-            next = key
-            push!(open, key)
-        end
-        verbose && println()
-    end
-    return total, sequence
 end
 
 function main(filename, seconds = 30, verbose = false)
     map, next = parseInput(filename)
     # println.(zip(keys(map), values(map)))
     n = length(keys(map))
-    t, sequence = notthebest(n, next, map, seconds, verbose)
-    # println(sequence)
-    return t
+    setofvalues = (values(map) .|> val -> val.flow)
+    best = setofvalues |> sum
+    distances = Dict{String, Dict{String, Int}}()
+    for k in keys(map) |> collect |> sort
+        # println(k, " => ", map[k])
+        distances[k] = bfs(map, k)
+    end
+    # @show distances
+    opened = []
+    s = 0
+    tot = 0
+    i = 1
+    operation = ""
+    steps = 0
+    while i <= seconds && !isnothing(next)
+        curr = next
+        next = findbest(map, distances, next, opened)
+        if !isnothing(next)
+            # now should i open or should i change?
+            # i will change
+            # 1 second to move from curr to next
+            # 1 second to open
+            # but i could need more than 1 second to move
+            # i spend a second for each move from node to node
+            println("== Minute $(i) ==")
+            msg = length(opened) == 0 ? "No valves are open." : "Valve $(join(opened, ", ")) are open"
+            msg *= s == 0 ? "" : ", releasing $(s) pressure" 
+            println(msg)
+            if operation == ""
+                println("You move to valve $(next).")
+                # but how long is the distance?
+                delta = distances[curr][next]
+                # println("That is at distance $(delta)")
+                # distance is stored in delta
+                operation = "open"
+                steps = delta
+                curr = next
+            elseif operation == "open"
+                if steps == 1
+                    println("You open valve $(curr).")
+                    push!(opened, curr)
+                    tot += map[next].flow * (seconds - (i+1))
+                    s += map[next].flow
+                    operation = ""
+                else
+                    steps -= 1
+                end
+            end
+            # push!(opened, )
+
+            
+            
+            
+            # println("distance from $(curr) to $(next) is $(delta)")
+            # println("delta $(delta)")
+            # seconds -= delta
+            # println("flow * seconds  = $(map[next].flow) * $(seconds)")
+            # println("best node from $(curr) is $(next)")
+        end
+        println()
+        i +=  1
+    end
+
+    return s, tot
 end
 
 @time println(main("easy.txt", 30))
-@time println(main("hard.txt", 30))
+# @time println(main("hard.txt", 30))
