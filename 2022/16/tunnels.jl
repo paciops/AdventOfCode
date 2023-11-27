cfcd189abd30f8fd1e62fa2173a23ca2a91d1d8a
riccardo pacioni <riccardo960@gmail.com>
2023-11-27 17:23:44 +0100

    day 16 part two done
    Signed-off-by: riccardo pacioni <riccardo960@gmail.com>

--- 2022/16/tunnels.jl
+++ 2022/16/tunnels.jl
@@ -1,4 +1,7 @@
 using DataStructures
+using ProgressMeter
+
+START = "AA"
 
 function parseValve(data::Vector{String})
     valves = findfirst(el -> el == "valves", data)
@@ -8,147 +11,98 @@ function parseValve(data::Vector{String})
     return replace.(data[valves+1:end], "," => "")
 end
 
-struct Node{T}
-    flow::Int
-    valves::Array{T}
-end
-
-struct Graph{T}
-
-end
-
-NodeMap{T} = Dict{String, Node{T}} where T
-
 function parseInput(filename::String)
-    map = NodeMap{String}()
-    start = nothing
+    valves = Dict{String, Int}()
+    tunnels = Dict{String, Array{String}}()
     open(filename) do file
         for line in eachline(file)
             data = String.(split(line, " "))
             name = data[2]
-            start = isnothing(start) ? name : start
             flow = parse(Int, replace(data[5], ";"=>" ", "rate="=>" "))
-            valves = parseValve(data)
-            node = Node{String}(flow, valves)
-            map[name] = node
+            targets = parseValve(data)
+            valves[name] = flow
+            tunnels[name] = targets
         end
     end
-    return map, start
+    return valves, tunnels
 end
 
-function bfs(map::NodeMap{T}, start::T) where T
-    visited = Set{T}()
-    queue = Vector([start])
-    distances = Dict{T, Int}(start => 0)
-    while !isempty(queue)
-        curr = popfirst!(queue)
-        distance = distances[curr]+1
-        for k in map[curr].valves
-            if !haskey(distances, k)
-                distances[k] = distance
-                push!(queue, k)
+function createdistance(valves::Dict{String, Int}, tunnels::Dict{String, Array{String}})
+    positivefloworstart = filter(((v,f),) -> v == START || f > 0, valves)
+    distances = Dict{String, Dict{String, Int}}()
+    for (valve, flow) in positivefloworstart
+        distances[valve] = Dict(valve => 0, START => 0)
+        visited = Set{String}([valve])
+        queue = Vector{Tuple{String, Int}}([(valve, 0)])
+        while !isempty(queue)
+            position, distance = popfirst!(queue)
+            for neighbor in tunnels[position]
+                if !(neighbor in visited)
+                    push!(visited, neighbor)
+                    if valves[neighbor] > 0
+                        distances[valve][neighbor] = distance + 1
+                    end
+                    push!(queue, (neighbor, distance + 1))
+                end
             end
+        end
+        delete!(distances[valve],valve)
+        if valve != START
+            delete!(distances[valve], START)
         end
     end
     return distances
 end
 
-function findbest(map, distances, key, opened, avoid)
-    best = 0.0
-    index = nothing
-    for (k, dist) in distances
-        if !(k in opened) && !(k in avoid)
-            valve = map[k]
-            d = dist[key]
-            d = d == 0 ? 1 : d
-            if valve.flow / d > best
-                best = valve.flow / d
-                index = k
-            end
+function cachedfs(time::Int, valve::T, mask::Set{T}, distances::Dict{T, Dict{T, Int}}, valves::Dict{T, Int}, cache = Dict{Tuple{Int, T, Set{T}}, Int}()) where T
+    if haskey(cache, (time, valve, mask))
+        return cache[(time, valve, mask)]
+    end
+    maxval = 0
+    for (neighbor, dist) in distances[valve]
+        remtime = time - dist - 1
+        if remtime > 0 && !(neighbor in mask)
+            maskcopy = copy(mask)
+            push!(maskcopy, neighbor)
+            maxval = max(maxval, cachedfs(remtime, neighbor, maskcopy, distances, valves, cache) + (valves[neighbor] * remtime))
         end
     end
-    return index
+    cache[(time, valve, mask)] = maxval
+    return maxval
 end
 
-function randomwalk(map, distances, next, n)
-    opened = Set()
-    s = 0
-    order = []
-    while n > 0
-        push!(order, next)
-        node = map[next]
-        if node.flow > 0 && !(next in opened)
-            s += node.flow
-            push!(opened, next)
-        end
-        next = rand(node.valves)
-        n -= 1
+function main(filename, minutes)
+    valves, tunnels = parseInput(filename)
+    distances = createdistance(valves, tunnels)
+    return cachedfs(minutes, START, Set{String}(), distances, valves)
+end
+
+function maintwo(filename, minutes)
+    valves, tunnels = parseInput(filename)
+    distances = createdistance(valves, tunnels)
+    positivevalves = filter(((v, f),)-> f > 0, valves) |> keys |> collect
+    cache = Dict{Tuple{Int, String, Set{String}}, Int}()
+    # first with increase empty and decrease full
+    m = 0
+    # @show m
+    n = length(positivevalves)
+    # n = 3
+    @showprogress for i = 0:((2^n)-1)/2
+        indexes = findall(el -> el == '1', UInt16(i) |> bitstring |> reverse)
+        # @show indexes
+        increase = Set{String}(map(i -> positivevalves[i], indexes))
+        decrease = Set{String}(filter(el -> !(el in increase), positivevalves))
+        # push!(increase, v)
+        # delete!(decrease, v)
+        m = max(m, cachedfs(minutes, START, increase, distances, valves, cache) + cachedfs(minutes, START, decrease, distances, valves, cache))
+        # @show m
+        # @show increase
+        # @show decrease
     end
-    return s, order
+    return m
 end
 
-function elementsat(paths::Vector{Vector{T}}, pos::Int) where T
-    if length(paths) == 0 || length(paths[1]) < pos
-        return Set{T}()
-    end
-    return Set(paths .|> el -> el[pos])
-end
-
-function findpath(map, distances, minutes, next, otherpaths::Vector{Vector{String}})
-    opened = Set{String}()
-    visited = String[]
-    i = 1
-    s = 0
-    tot = 0
-    while !isnothing(next)
-        push!(visited, next)
-        node = map[next]
-        if node.flow > 0 && !(next in opened)
-            push!(opened, next)
-            s += node.flow
-            tot += node.flow * (minutes-i)
-            println("Minute $(i) node $(next) $(node.flow) * $(minutes-i)")
-        end
-        prev = next
-        avoid = elementsat(otherpaths, length(visited)+1)
-        println("Keys to avoid $(avoid)")
-        next = findbest(map, distances, next, opened, avoid)
-        if !isnothing(next)
-            i += distances[prev][next] + 1
-        end
-    end    
-    return visited, s, tot
-end
-
-function main(filename, minutes = 30, verbose = false)
-
-    @assert elementsat([[1]],1) == Set([1]) "Result is $(elementsat([[1]],1)) instead of $(Set([1]))"
-    @assert elementsat([[1,3,4,5], [2,3,4,5]],1) == Set([1,2]) "Result is $(elementsat([[1]],1)) instead of $(Set([1]))"
-
-
-    map, next = parseInput(filename)
-    # println.(zip(keys(map), values(map)))
-    n = length(keys(map))
-    distances = Dict{String, Dict{String, Int}}()
-    for k in keys(map) |> collect |> sort
-        # println(k, " => ", map[k])
-        distances[k] = bfs(map, k)
-    end
-    paths = Vector{Vector{String}}()
-    bests = 0
-    besttot = 0
-    for _ = 1:10
-        path, s, tot = findpath(map, distances, minutes, next, paths)
-        push!(paths, path)
-        if tot > besttot
-            besttot = tot
-            bests = s
-        end
-        println()
-    end
-    println(path)
-    return bests, besttot
-end
-
-@time println(main("easy.txt", 30))
-# @time println(main("hard.txt", 30))
+# @time println("easy\t", main("easy.txt", 30))
+# @time println("hard\t", main("hard.txt", 30))
+@time println("easy\t", maintwo("easy.txt", 26))
+@time println("hard\t", maintwo("hard.txt", 26))
