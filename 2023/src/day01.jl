306f915782feaca02cd74e41e76f620a289bdd7a
riccardo pacioni <riccardo960@gmail.com>
2023-12-02 20:36:55 +0100

    [2023] day 1 solved
    Signed-off-by: riccardo pacioni <riccardo960@gmail.com>

--- 2023/src/day01.jl
+++ 2023/src/day01.jl
@@ -2,9 +2,33 @@ module Day1
 
 using AdventOfCode2023
 
-function solve(input::String = AdventOfCode2023.readinput(1))
-    @show input
-    return 0
+wordmap = ["one" => "1", "two" => "2", "three" => "3",
+    "four" => "4", "five" => "5", "six" => "6", "seven" => "7",
+    "eight" => "8", "nine" => "9"]
+
+wordkeys = map(first, wordmap)
+
+function solve(input::String = AdventOfCode2023.readinput(1); partTwo = false)
+    acc = 0
+    if partTwo
+        specialmap = Vector{Pair{String, String}}()
+        for a in wordkeys
+            for b in wordkeys
+                if a[end] == b[1]
+                    push!(specialmap, (a) * (b[2:end]) => "$(findfirst(el -> first(el) == a, wordmap))$(findfirst(el -> first(el) == b, wordmap))")
+                end
+            end
+        end
+    end
+    for l in split(input, '\n') .|> String
+        isempty(l) && continue
+        if partTwo
+            l = Base.replace(l, specialmap...)
+            l = Base.replace(l, wordmap...)
+        end
+        acc += filter(isdigit, l) |> list -> parse(Int, list[1] * list[end])
+    end
+    return acc
 end
 
 end
--- 2023/test/runtest.jl
+++ 2023/test/runtest.jl
@@ -1,6 +1,12 @@
 using AdventOfCode2023
 using Test
 
+testfile = (i) -> AdventOfCode2023.readinput(abspath(joinpath(@__DIR__, "..", "data", "test$(i).txt")))
+
 @testset "Day 1" begin
-    @test AdventOfCode2023.Day1.solve() == 0
+    inputtest = testfile(1)
+    @test AdventOfCode2023.Day1.solve(inputtest) == 142
+    println("Result day 1 part 1:\t", AdventOfCode2023.Day1.solve())
+    @test AdventOfCode2023.Day1.solve(testfile("1.1"); partTwo=true) == 281
+    println("Result day 1 part 2:\t", AdventOfCode2023.Day1.solve(;partTwo=true))
 end
